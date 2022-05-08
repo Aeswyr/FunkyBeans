4f87b905b1a0cb0482cef4f900834bdc19ba8de8
pascal <pmkgbwis@gmail.com>
2022-05-08 04:10:30 -0500

    Movement and tileset experimentation

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 dir {
+        get;
+        private set;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext ctx) {
+        dir = ctx.ReadValue<Vector2>();
+    }
+}
+
+public struct Button {
+    public bool down, released, pressed;
+
+    public void Reset() {
+        released = false;
+        pressed = false;
+    }
+
+    public void Set(InputAction.CallbackContext ctx) {
+        down = ctx.canceled == false;
+
+        released = !down;
+        pressed = down;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rbody;
+    [SerializeField] private InputHandler input;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbody.velocity = speed * input.dir;
+    }
+}
